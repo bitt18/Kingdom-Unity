0863b65608a792a422462e91902e75181e07522d
bitt18 <125684554+bitt18@users.noreply.github.com>
2023-03-11 22:28:22 +0530

    Delete Rough 2.py

--- Rough 2.py
+++
@@ -1,9 +0,0 @@
-noofroads=8
-listofcities=[{1,2},{2,4},{1,6},{2,3},{0,6},{4,5},{0,7},{5,6}]
-for p in range(noofroads):
-    complement = set()
-    for q in listofcities:
-        if(p in q):
-            r=q-{p}
-            complement.add(max(r))
-    print(p, complement)
