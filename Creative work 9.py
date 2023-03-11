fd50a203d926aab09b0d9f83d7a5c59eb5c4f3ea
bitt18 <125684554+bitt18@users.noreply.github.com>
2023-03-11 22:27:08 +0530

    Delete Creative work 9.py

--- Creative work 9.py
+++
@@ -1,6 +0,0 @@
-cities,noofroads,cost=map(int,input().split())
-roadlist=[]
-for i in range(noofroads):
-    roadindex=a=list(map(int,input().split()))
-    roadlist.append(roadindex)
-print(roadlist)
