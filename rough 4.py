df784e47c540bb1b031219f0fd22246a00f9de24
bitt18 <125684554+bitt18@users.noreply.github.com>
2023-03-11 22:23:37 +0530

    Delete rough 4.py

--- rough 4.py
+++
@@ -1,9 +0,0 @@
-noofroads=6
-l=[{0,1},{1,2},{3,4},{2,4},{2,6},{5,2}]
-for p in range(noofroads):
-    check = set()
-    for q in l:
-        if(p in q):
-            r=q-{p}
-            check.add(max(r))
-    print(p, check)
